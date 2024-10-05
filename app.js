38e38f9c4c249e4bd915b199804566df86b33994
Lasha Bidzinashvili <116507932+Bidzinashvili@users.noreply.github.com>
2024-10-05 22:22:12 +0400

    .

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express"
 import bodyParser from "body-parser"
+import cors from "cors"
 
 const app = express()
 const port = 3001
 
 app.use(bodyParser.json())
+app.use(cors())
 
 import HomeRoutes from "./routes/HomeRoutes.js"
 app.use('/', HomeRoutes)

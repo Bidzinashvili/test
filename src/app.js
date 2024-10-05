8643cd5d752ea551d8d562135e3ba2960fc81fbd
Lasha Bidzinashvili <116507932+Bidzinashvili@users.noreply.github.com>
2024-10-05 22:01:51 +0400

    .

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import express from "express"
 import bodyParser from "body-parser"
+import ServerlessHttp from "serverless-http"
 
 const app = express()
 const port = 3001
 
 app.use(bodyParser.json())
 
-import HomeRoutes from "./routes/HomeRoutes.js"
+import HomeRoutes from "../routes/HomeRoutes.js"
 app.use('/', HomeRoutes)
 
 app.listen(port, () => {
   console.log(`App running on localhost:${port}`)
 })
+
+module.exports = ServerlessHttp(app)
